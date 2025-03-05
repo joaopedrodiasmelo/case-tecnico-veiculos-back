d361c746caaee6f56bedb6a0b52b8e37ed8e5c8e
joao pedro <dias.jpmelo@usp.br>
2025-03-05 09:24:05 -0300

    feat: Criação do Projeto

---
+++ case-veiculos-jr/src/main/java/com/joaopedro/case_veiculos_jr/CaseVeiculosJrApplication.java
@@ -0,0 +1,13 @@
+package com.joaopedro.case_veiculos_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseVeiculosJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseVeiculosJrApplication.class, args);
+	}
+
+}
---
+++ case-veiculos-jr/src/test/java/com/joaopedro/case_veiculos_jr/CaseVeiculosJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopedro.case_veiculos_jr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseVeiculosJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
